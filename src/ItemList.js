f267f5380fa3cc32bd1fc416f0131eec4fd800d2
Dylan McLane <mclane_dylan@yahoo.com>
2023-08-21 11:42:47 -0700

    Update ItemList.js

--- src/ItemList.js
+++ src/ItemList.js
@@ -8,7 +8,7 @@ function ItemList() {
   useEffect(() => {
     fetch(`https://raw.githubusercontent.com/dylanmclane/HikingSupplyCo/main/src/db.json`)
       .then((response) => response.json())
-      .then((data) => setItems(data));
+      .then((data) => setItems(data.items));
   }, []);
 
   return (
