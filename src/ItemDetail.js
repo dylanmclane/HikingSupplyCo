da41307cd2f3ac7bdd6cb72fea8909b71c929ad2
Dylan McLane <mclane_dylan@yahoo.com>
2023-08-21 16:23:59 -0700

    Update fetch URL

--- src/ItemDetail.js
+++ src/ItemDetail.js
@@ -39,7 +39,7 @@ function ItemDetail({ addToCart }) {
 
   // fetch data for specific item id from mock REST API json
   useEffect(() => {
-    fetch(`/netlify/functions/processJSON?id=${id}`)
+    fetch(`https://hikingsupplyco.netlify.app/.netlify/functions/processJSON?id=${yourItemId}`)
       .then((response) => response.json())
       .then((data) => setItem(data));
   }, [id]);
