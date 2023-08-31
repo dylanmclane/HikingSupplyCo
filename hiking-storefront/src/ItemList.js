284977d0c2757637e30f1ca2bd16442b5251979a
dylanmclane <mclane_dylan@yahoo.com>
2023-08-31 15:04:59 -0700

    Added filter dropdown feature

--- hiking-storefront/src/ItemList.js
+++ hiking-storefront/src/ItemList.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 function ItemList() {
   const [items, setItems] = useState([]);
+  const [selectedType, setSelectedType] = useState('');
 
   // fetch data for list of items from mock REST API json
   useEffect(() => {
@@ -11,9 +12,21 @@ function ItemList() {
       .then((data) => setItems(data));
   }, []);
 
+  const itemTypes = [...new Set(items.map(item => item.type))];
+  const filteredItems = selectedType ? items.filter(item => item.type === selectedType) : items;
+
   return (
     <div>
-      {items.map((item) => (
+      <select 
+          value={selectedType} 
+          onChange={e => setSelectedType(e.target.value)}>
+          <option value="">All Types</option>
+          {itemTypes.map(type => (
+              <option key={type} value={type}>{type}</option>
+          ))}
+      </select>
+
+      {filteredItems.map((item) => (
         <div className="item" key={item.id}>
           <h3><Link to={`/item/${item.id}`}>{item.name}</Link></h3>
         </div>
