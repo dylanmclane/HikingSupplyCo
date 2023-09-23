781776a7d690dcd468cd779f69ed0808639086a6
mclane0209 <mclane_dylan@yahoo.com>
2023-09-22 20:19:16 -0700

    Added styles and updated shopping cart logic Added styles to shopping cart component. Updated shopping cart logic to combine multiples of the same item into a single item. Multiples now represented by a number.

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,6 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function ShoppingCart({ cart, setCart }) {
+
+  // State for summarized cart
+  const [summarizedCart, setSummarizedCart] = useState([]);
+
+  useEffect(() => {
+    const summary = cart.reduce((acc, item) => {
+      // Find existing item in accumulator
+      const existingItem = acc.find(accItem => accItem.id === item.id);
+      
+      if (existingItem) {
+        // Increase the count
+        existingItem.count += 1;
+      } else {
+        // Add new item with count property
+        acc.push({...item, count: 1});
+      }
+
+      return acc;
+    }, []);
+
+    setSummarizedCart(summary);
+  }, [cart]);
   
   // Calculate total price
   const totalPrice = cart.reduce((acc, item) => acc + parseFloat(item.price.slice(1)), 0);
@@ -18,16 +40,17 @@ function ShoppingCart({ cart, setCart }) {
   };
 
   return (
-    <div>
+    <div className="cart">
       <h2>Your Shopping Cart</h2>
-      {cart.length === 0 ? <p>Your cart is empty</p> : cart.map((item, index) => (
-        <div key={index}>
-          <p>{item.name}</p>
+      {summarizedCart.length === 0 ? <p>Your cart is empty</p> : summarizedCart.map((item, index) => (
+        <div key={index} className="cart-item">
+          <img src={`https://raw.githubusercontent.com/dylanmclane/HikingSupplyCo/main/src/images/${item.image}`} alt={item.name} className="thumbnail" />
+          <p>{item.name} (x{item.count})</p>
         </div>
       ))}
-      <p>Total: ${totalPrice.toFixed(2)}</p>
+      <p className="total-price">Total: ${totalPrice.toFixed(2)}</p>
       {cart.length > 0 && (
-        <button onClick={handleEmptycart}>Empty Cart</button>
+        <button onClick={handleEmptycart} className="cart-button">Empty Cart</button>
       )}
       {showPopup && <div className="popup">Cart emptied!</div>}
     </div>
